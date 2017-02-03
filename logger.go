924216302d549c2cc89bf5ceb652c018a0fd54e0
Kousaka Kazuma <kazu1107@gmail.com>
2017-02-03 17:21:19 +0900

    initialize commit

---
+++ logger.go
@@ -0,0 +1,257 @@
+package el
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+type LogLevel int
+
+const (
+	FATAL = iota
+	ERROR
+	WARN
+	INFO
+	TRACE
+)
+
+var levelTag = []string{
+	"[FATAL]",
+	"[ERROR]",
+	"[WARN]",
+	"[INFO]",
+	"[TRACE]",
+}
+
+type logStatus struct {
+	Debug       bool
+	LogLevel    LogLevel
+	RotateLevel RotateLevel
+	Rotator     *Rotator
+	chRotate    chan *os.File
+	chExit      chan bool
+}
+
+var state = &logStatus{
+	LogLevel:    WARN,
+	RotateLevel: RNone,
+	chRotate:    make(chan *os.File),
+	chExit:      make(chan bool),
+}
+
+var syncMtx = new(sync.Mutex)
+
+func SetLogLevel(level LogLevel) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+	state.LogLevel = level
+}
+
+func SetDebug() {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+	state.Debug = true
+}
+
+func SetRelease() {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+	state.Debug = false
+}
+
+func File() *os.File {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	return state.Rotator.File()
+}
+
+func SetRotateLog(path string) error {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if state.Rotator != nil {
+		state.Rotator.Close()
+	} else {
+		go watch()
+
+	}
+
+	r, err := NewRotator(path)
+	if err != nil {
+		return err
+	}
+
+	state.Rotator = r
+
+	r.SetRotateChannel(r.chRotate)
+
+	return nil
+}
+
+func Close() {
+	state.chExit <- true
+	<-state.chExit
+	state.Rotator.Close()
+}
+
+func watch() {
+Exit:
+	for {
+		select {
+		case <-state.chRotate:
+
+		case <-state.chExit:
+			break Exit
+		}
+	}
+
+	state.chExit <- true
+}
+
+func Println(l LogLevel, vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if l > state.LogLevel {
+		return
+	}
+	vals = append([]interface{}{levelTag[l]}, vals...)
+	fmt.Fprintln(state.Rotator.File(), vals...)
+}
+
+func Printf(l LogLevel, f string, vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if l > state.LogLevel {
+		return
+	}
+	fmt.Fprintf(state.Rotator.File(), levelTag[l]+" "+f, vals...)
+}
+
+func Fatal(vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	outputln(FATAL, vals)
+
+	os.Exit(-1)
+}
+
+func Fatalf(f string, vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+	outputf(FATAL, f, vals)
+	os.Exit(-1)
+}
+
+func Error(vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+	if ERROR > state.LogLevel {
+		return
+	}
+
+	outputln(ERROR, vals)
+}
+
+func Errorf(f string, vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if ERROR > state.LogLevel {
+		return
+	}
+	outputf(ERROR, f, vals)
+}
+
+func Warn(vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if WARN > state.LogLevel {
+		return
+	}
+	outputln(WARN, vals)
+}
+
+func Warnf(f string, vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if WARN > state.LogLevel {
+		return
+	}
+	outputf(WARN, f, vals)
+}
+
+func Info(vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if INFO > state.LogLevel {
+		return
+	}
+	outputln(INFO, vals)
+}
+
+func Infof(f string, vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if INFO > state.LogLevel {
+		return
+	}
+	outputf(INFO, f, vals)
+}
+
+func Trace(vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if TRACE > state.LogLevel {
+		return
+	}
+	outputln(TRACE, vals)
+}
+
+func Tracef(f string, vals ...interface{}) {
+	syncMtx.Lock()
+	defer syncMtx.Unlock()
+
+	if TRACE > state.LogLevel {
+		return
+	}
+	outputf(TRACE, f, vals)
+}
+
+func outputln(level LogLevel, vals []interface{}) {
+	now := time.Now().Format("2006-01-02 15:04:05")
+	vals = append([]interface{}{now, levelTag[level]}, vals...)
+	if state.Debug {
+		f := makeFormat(vals)
+		fmt.Fprintf(state.Rotator.File(), f+"\n", vals...)
+	} else {
+		fmt.Fprintln(state.Rotator.File(), vals...)
+	}
+}
+
+func outputf(level LogLevel, f string, vals []interface{}) {
+	now := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Fprintf(state.Rotator.File(), now+" "+levelTag[level]+f, vals...)
+}
+
+func makeFormat(vals []interface{}) string {
+	f := "%s"
+	for _, v := range vals[1:] {
+		if _, ok := v.(error); ok {
+			f += " %+v"
+		} else {
+			f += " %v"
+		}
+	}
+	return f
+}
---
+++ rotator.go
@@ -0,0 +1,133 @@
+package el
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type RotateLevel int
+
+const (
+	RDay = iota
+	RMonth
+	RYear
+	RNone
+)
+
+type Rotator struct {
+	PathFormat  string
+	file        *os.File
+	rotateLevel RotateLevel
+	chRotate    chan<- *os.File
+	chExit      chan bool
+}
+
+func NewRotator(path string) (*Rotator, error) {
+
+	var rLevel RotateLevel = RNone
+
+	if strings.Index(path, "%D") != -1 {
+		rLevel = RDay
+	} else if strings.Index(path, "%M") != -1 {
+		rLevel = RMonth
+	} else if strings.Index(path, "%Y") != -1 {
+		rLevel = RYear
+	}
+
+	result := &Rotator{
+		PathFormat:  path,
+		rotateLevel: rLevel,
+		chRotate:    nil,
+		chExit:      make(chan bool),
+	}
+
+	now := time.Now()
+	if err := result.changeLogFile(now); err != nil {
+		return nil, err
+	}
+
+	go result.watch(&now)
+
+	return result, nil
+}
+
+func (r *Rotator) File() *os.File {
+	return r.file
+}
+
+func (r *Rotator) SetRotateChannel(ch chan<- *os.File) {
+	r.chRotate = ch
+}
+
+func (r *Rotator) changeLogFile(t time.Time) error {
+
+	// open new log
+	filePath := logFilePath(r.PathFormat, t)
+	lf, err := os.OpenFile(filePath,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+
+	if r.chRotate != nil {
+		r.chRotate <- lf
+	}
+
+	if r.file != nil {
+		r.file.Close()
+	}
+
+	r.file = lf
+
+	return nil
+}
+
+func (r *Rotator) watch(lastDay *time.Time) {
+
+	ticker := time.NewTicker(time.Second)
+
+Exit:
+	for {
+		select {
+		case <-ticker.C:
+			t := time.Now()
+
+			switch r.rotateLevel {
+			case RDay:
+				if lastDay.Day() == t.Day() {
+					continue
+				}
+			case RMonth:
+				if lastDay.Month() == t.Month() {
+					continue
+				}
+			case RYear:
+				if lastDay.Year() == t.Year() {
+					continue
+				}
+			}
+			if r.changeLogFile(t) != nil {
+				Error("Failed rotate log file")
+			}
+			lastDay = &t
+		case <-r.chExit:
+			break Exit
+		}
+	}
+	ticker.Stop()
+	r.chExit <- true
+}
+
+func (r *Rotator) Close() {
+	r.chExit <- true
+	<-r.chExit
+}
+
+func logFilePath(path string, t time.Time) string {
+	path = strings.Replace(path, "%D", fmt.Sprintf("%02d", t.Day()), -1)
+	path = strings.Replace(path, "%M", fmt.Sprintf("%02d", t.Month()), -1)
+	path = strings.Replace(path, "%Y", fmt.Sprintf("%02d", t.Year()), -1)
+	return path
+}
